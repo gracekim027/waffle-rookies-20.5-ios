f08c14a4c23506a7d0aa878b7f9ba33707fdecee
gracekim027 <82282599+gracekim027@users.noreply.github.com>
2022-11-12 17:59:46 +0900

    added branch

---
+++ seminar-3-assignment/SearchMovies/SearchMovies/Customs/Custom Tab Bar/CustomTabBar 2.swift
@@ -0,0 +1,85 @@
+//
+//  CustomTabBar.swift
+//  CustomTabBarExample
+//
+//  Created by Jędrzej Chołuj on 18/12/2021.
+//
+import UIKit
+import RxSwift
+import RxCocoa
+import RxGesture
+
+final class CustomTabBar: UIStackView {
+    
+    var itemTapped: Observable<Int> { itemTappedSubject.asObservable() }
+    
+    private lazy var customItemViews: [CustomItemView] = [homeItem, favoriteItem]
+    
+    private let homeItem = CustomItemView(with: .home, index: 0)
+    private let favoriteItem = CustomItemView(with: .favorite, index: 1)
+    
+    private let itemTappedSubject = PublishSubject<Int>()
+    private let disposeBag = DisposeBag()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupHierarchy()
+        setupProperties()
+        bind()
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+        selectItem(index: 0)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupHierarchy() {
+        addArrangedSubviews([homeItem, favoriteItem])
+    }
+    
+    private func setupProperties() {
+        distribution = .fillEqually
+        alignment = .center
+        
+        backgroundColor = Styles.backgroundBlue
+        setupCornerRadius(30)
+        
+        customItemViews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            //$0.clipsToBounds = true
+        }
+    }
+    
+    private func selectItem(index: Int) {
+        customItemViews.forEach { $0.isSelected = $0.index == index }
+        itemTappedSubject.onNext(index)
+    }
+    
+    //MARK: - Bindings
+    
+    private func bind() {
+        homeItem.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                self.homeItem.animateClick {
+                    self.selectItem(index: self.homeItem.index)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        favoriteItem.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                self.favoriteItem.animateClick {
+                    self.selectItem(index: self.favoriteItem.index)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+}
